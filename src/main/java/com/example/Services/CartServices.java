7cf7d23c24123de36ed113432aeecb0e050b9cdc
emanuel <emanueldionicio809@gmail.com>
2022-07-09 21:16:06 -0600

    se creo el servicio del carro de compras

---
+++ src/main/java/com/example/Services/CartServices.java
@@ -0,0 +1,15 @@
+package com.example.Services;
+
+import java.util.List;
+
+import com.example.Entities.Cart;
+
+
+
+public interface CartServices {
+	List<Cart> list();
+	Cart search(Long id);
+	Cart add(Cart cart);
+	Cart update(Long id, Cart cart);
+	void delete(Long id);
+}
