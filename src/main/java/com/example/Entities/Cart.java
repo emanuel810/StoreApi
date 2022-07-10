70b7e6d0b864ce634f6be339fadd6f510b9e13a1
emanuel <emanueldionicio809@gmail.com>
2022-07-09 20:55:07 -0600

    se creo la entidad carro de compras

---
+++ src/main/java/com/example/Entities/Cart.java
@@ -0,0 +1,35 @@
+package com.example.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "cart_Shopping")
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cart_id;
+	
+	@NotNull(message = "fill in this field")
+	private Long client_id;
+	
+	@NotNull(message = "fill in this field")
+	private Long product_id;
+
+	public Long getCart_id() {
+		return cart_id;
+	}
+
+	public Long getClient_id() {
+		return client_id;
+	}
+
+	public Long getProduct_id() {
+		return product_id;
+	}
+}
