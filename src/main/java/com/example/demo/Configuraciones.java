fddf8ae7916926aa322bf62aaf77d95765ff0a7e
emanuel <emanueldionicio809@gmail.com>
2022-07-09 20:48:53 -0600

    se eliminaron los permisos del cors

--- src/main/java/com/example/demo/Configuraciones.java
+++ src/main/java/com/example/demo/Configuraciones.java
@@ -1,8 +1,16 @@
 package com.example.demo;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,7 +22,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Configuration
 @Component
-public class Configuraciones {
+public class Configuraciones extends OncePerRequestFilter{
 	@Bean
 	public Docket api(){
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -32,5 +40,18 @@ public class Configuraciones {
 				.title("Polar City")
 				.description("Api para agregar producto y usuarios con carrito incluido")
 				.build() ;
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addIntHeader("Access-Control-Max-Age", 10);
+        filterChain.doFilter(request, response);
 	}
 }
